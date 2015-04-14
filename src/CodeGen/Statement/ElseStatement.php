b8d7fd76e8036c5cfad3d6d8a2a526378f828a83
c9s <yoanlin93@gmail.com>
2015-04-14 21:33:20 +0800

    Add ElseStatement

---
+++ src/CodeGen/Statement/ElseStatement.php
@@ -0,0 +1,26 @@
+<?php
+namespace CodeGen\Statement;
+use CodeGen\Statement\Statement;
+use CodeGen\Block;
+use CodeGen\Renderable;
+use CodeGen\VariableDeflator;
+use CodeGen\Utils;
+
+class ElseStatement extends Block implements Renderable
+{
+    public $else;
+
+    public function __construct($block)
+    {
+        $this->else = Utils::evalCallback($block);
+    }
+
+    public function render(array $args = array()) 
+    {
+        $this->else->setIndentLevel($this->indentLevel + 1);
+        $this[] = ' else {';
+        $this[] = $this->else;
+        $this[] = '}';
+        return parent::render($args);
+    }
+}
