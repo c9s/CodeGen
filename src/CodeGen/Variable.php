5a1c5943cebcb6c434449e4adf966a36dbf31f59
c9s <yoanlin93@gmail.com>
2015-07-04 09:46:24 +0800

    Allow variable name template

--- src/CodeGen/Variable.php
+++ src/CodeGen/Variable.php
@@ -1,18 +1,36 @@
 <?php
 namespace CodeGen;
 use CodeGen\Renderable;
+use CodeGen\Utils;
 
 class Variable implements Renderable
 {
     protected $name;
+
+    protected $applyTemplate = false;
 
     public function __construct($name)
     {
         $this->name = $name;
     }
 
+    public function setApplyTemplate($applyTemplate = true) 
+    {
+        $this->applyTemplate = $applyTemplate;
+    }
+
+    static public function template($name)
+    {
+        $var = new self($name);
+        $var->setApplyTemplate(true);
+        return $var;
+    }
+
     public function render(array $args = array()) 
     {
+        if ($this->applyTemplate) {
+            return Utils::renderStringTemplate($this->name, $args);
+        }
         return $this->name;
     }
 }
