a8199d5b1ee96ecc124d6e0a0f63f9f43626a799
c9s <yoanlin93@gmail.com>
2015-07-04 09:42:18 +0800

    cache twig object

--- src/CodeGen/Utils.php
+++ src/CodeGen/Utils.php
@@ -6,25 +6,33 @@ use Closure;
 
 class Utils
 {
-    static public function renderStringTemplate($templateContent, $args = array()) {
-        $loader = new Twig_Loader_String();
-        $twig = new Twig_Environment($loader);
+    static $stringloader = null;
+    static $twig;
 
-        if ( is_callable($args) ) {
+    static public function renderStringTemplate($templateContent, array $args = array()) 
+    {
+        if (!self::$stringloader) {
+            self::$stringloader = new Twig_Loader_String();
+        }
+        if (!self::$twig) {
+            self::$twig = new Twig_Environment(self::$stringloader);
+        }
+        if (is_callable($args)) {
             $args = call_user_func($args);
-        } elseif ( $args instanceof Closure ) {
+        } elseif ($args instanceof Closure) {
             $args = $args();
         }
-        return $twig->render($templateContent, $args);
+        return self::$twig->render($templateContent, $args);
     }
 
-    static public function evalCallback($cb) {
+    static public function evalCallback($cb) 
+    {
         return is_callable($cb) ? $cb() : $cb;
     }
 
     static public function indent($indent = 1, $spaces = 4)
     {
-        return str_repeat(' ', $spaces * $indent );
+        return str_repeat(' ', $spaces * $indent);
     }
 }
 
