a81fa429d064439d1eed39d383f1ca7ddb0ebc63
c9s <yoanlin93@gmail.com>
2015-07-15 11:51:56 +0800

    Add CallExpr class

---
+++ src/CodeGen/Expr/CallExpr.php
@@ -0,0 +1,80 @@
+<?php
+namespace CodeGen\Expr;
+use Exception;
+use CodeGen\Renderable;
+use CodeGen\Raw;
+use CodeGen\VariableDeflator;
+use CodeGen\ArgumentList;
+use LogicException;
+
+/**
+ * CallExpr is basically the same thing as method call, 
+ * but it allows you to change the method call operator.
+ */
+class CallExpr implements Renderable
+{
+    /**
+     * @var Variable|string
+     */
+    public $objectName;
+
+    public $method;
+
+    public $arguments;
+
+    protected $op;
+
+    public function __construct($objectName = null, $op = '->', $method, array $arguments = array())
+    {
+        $this->objectName = $objectName;
+        $this->op = $op;
+        $this->method = $method;
+        $this->arguments = new ArgumentList($arguments);
+    }
+
+    public function method($method)
+    {
+        $this->method = $method;
+        return $this;
+    }
+
+    public function op($op)
+    {
+        $this->op = $op;
+        return $this;
+    }
+
+    public function setArguments(array $args)
+    {
+        $this->arguments = new ArgumentList($args);
+    }
+
+    public function addArgument($arg)
+    {
+        $this->arguments[] = $arg;
+        return $this;
+    }
+
+    public function render(array $args = array())
+    {
+        $out = '';
+        if ($this->objectName) {
+            if ($this->objectName instanceof Renderable) {
+                $out .= $this->objectName->render($args);
+            } else {
+                $out .= $this->objectName;
+            }
+            $out .= $this->op;
+        }
+        $out .= $this->method . '(' . $this->arguments->render($args) . ')';
+        return $out;
+    }
+
+    public function __toString() {
+        return $this->render();
+    }
+    
+}
+
+
+
