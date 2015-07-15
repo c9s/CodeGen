6698484cc9cf2c606e9e62aeec82ec20a6f86fb2
c9s <yoanlin93@gmail.com>
2015-07-15 11:59:34 +0800

    Add static method call generator test

--- tests/CodeGen/Expr/CallExprTest.php
+++ tests/CodeGen/Expr/CallExprTest.php
@@ -12,6 +12,13 @@ class CallExprTest extends CodeGenTestCase
         $this->assertCodeEquals('$foo->bar();', $statement);
     }
 
+    public function testStaticMethodCall()
+    {
+        $call = new CallExpr('Some','::','bar');
+        $statement = new Statement($call);
+        $this->assertCodeEquals('Some::bar();', $statement);
+    }
+
     public function testFunctionCall()
     {
         $call = new CallExpr(null,null,'spl_autoload_register');
