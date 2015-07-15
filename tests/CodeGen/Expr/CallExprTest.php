6e43d00e0b4a76d14a4520f742ef1c401cd8c62d
c9s <yoanlin93@gmail.com>
2015-07-15 11:58:55 +0800

    Add call expr test

---
+++ tests/CodeGen/Expr/CallExprTest.php
@@ -0,0 +1,22 @@
+<?php
+use CodeGen\Expr\CallExpr;
+use CodeGen\Testing\CodeGenTestCase;
+use CodeGen\Statement\Statement;
+
+class CallExprTest extends CodeGenTestCase
+{
+    public function testSimpleMethodCall()
+    {
+        $call = new CallExpr('$foo','->','bar');
+        $statement = new Statement($call);
+        $this->assertCodeEquals('$foo->bar();', $statement);
+    }
+
+    public function testFunctionCall()
+    {
+        $call = new CallExpr(null,null,'spl_autoload_register');
+        $statement = new Statement($call);
+        $this->assertCodeEquals('spl_autoload_register();', $statement);
+    }
+}
+
