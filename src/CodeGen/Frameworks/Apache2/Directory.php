4f30b7ee03ef22aa8e630aaf4ff491726bd6d810
c9s <yoanlin93@gmail.com>
2017-02-05 23:58:55 +0800

    Update test fixture

--- src/CodeGen/Frameworks/Apache2/Directory.php
+++ src/CodeGen/Frameworks/Apache2/Directory.php
@@ -1,20 +1,25 @@
 <?php
 namespace CodeGen\Frameworks\Apache2;
+
 class Directory
     extends \CodeGen\Frameworks\Apache2\DirectoryDirectiveGroup
 {
+
     public function setPath($entry)
     {
         $this->path = $entry;
     }
+
     public function getPath()
     {
         return $this->path;
     }
+
     public function addOption($entry)
     {
         $this->options[] = $entry;
     }
+
     public function removeOption($entry)
     {
         $pos = array_search($entry, $this->options, true);
@@ -24,26 +29,32 @@ class Directory
         }
         return false;
     }
+
     public function setOptions(array $entries)
     {
         $this->options = $entries;
     }
+
     public function getOptions()
     {
         return $this->options;
     }
+
     public function setAllowOverride($entry)
     {
         $this->allowOverride = $entry;
     }
+
     public function getAllowOverride()
     {
         return $this->allowOverride;
     }
+
     public function setRequire($entry)
     {
         $this->require = $entry;
     }
+
     public function getRequire()
     {
         return $this->require;
--- tests/Foo2Class.php
+++ tests/Foo2Class.php
@@ -1,5 +1,6 @@
 <?php
 namespace Bar;
+
 class Foo2Class
 {
 }
